2a7620f4f5ce0b09f618338fcda4e158f87d65e6
Twoixter <josemiguel@perezruiz.com>
2010-03-17 16:32:48 +0100

    Polymorhpism enabled for embedded documents

--- lib/mongoid/extensions/hash/assimilation.rb
+++ lib/mongoid/extensions/hash/assimilation.rb
@@ -18,7 +18,7 @@ module Mongoid #:nodoc:
         #
         # Returns: The child +Document+.
         def assimilate(parent, options, type = nil)
-          klass = type ? type : options.klass
+          klass = type ? type : (self["_type"] ? self["_type"].constantize : options.klass)
           child = klass.instantiate(:_parent => parent)
           child.write_attributes(self.merge("_type" => klass.name))
           child.identify
